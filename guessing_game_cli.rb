e4a8f67aa2067c37af896c3cd12553e1d050602e
Phil Haines <phil.haines92@gmail.com>
2021-01-16 16:35:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,10 @@
 def run_guessing_game
   loop do
+    
       puts "Guess a number between 1 and 6."
       guess = gets.chomp
       computer_number = rand(1..6)
+      
       if guess == "exit"
         puts "Goodbye!"
         break
