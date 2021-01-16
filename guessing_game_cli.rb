8dfcb5d539190f22963bcb8cfff6db4c31e378f5
Phil Haines <phil.haines92@gmail.com>
2021-01-16 16:13:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,18 +2,9 @@ require "pry"
 
 def run_guessing_game
   
-  def generate_num
     num = rand(1..6)
-  end
-  
-  def get_user_input
-    puts "Guess number from 1 to 6"
     user_input = gets.chomp
-    return user_input
-  end
-  
-  def compare_guess (num, user_input)
-    binding.pry
+    
     if user_input == "exit"
       puts "Goodbye!"
     elsif user_input == num
@@ -21,11 +12,6 @@ def run_guessing_game
     elsif user_input != num
       puts "Sorry! The computer guessed #{num}"
     end
-    
-  end
   
-  generate_num
-  get_user_input
-  compare_guess(num, user_input)
 end
 
