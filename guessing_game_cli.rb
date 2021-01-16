879fd330369a2ef2566a08247e7cb4401dc6fd31
Phil Haines <phil.haines92@gmail.com>
2021-01-16 16:18:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     
     if user_input == "exit"
       output_statement = "Goodbye!"
-      return output_statement
+      puts output_statement
     elsif user_input == num
       puts "You guessed the correct number!"
     elsif user_input != num
