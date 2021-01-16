6539d726f101565533f269acc7f170918bda790b
Phil Haines <phil.haines92@gmail.com>
2021-01-16 16:29:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,6 @@ def run_guessing_game
       
       if user_input.to_i == num
         puts "You guessed the correct number!"
-        break
       elsif user_input.to_i != num
          puts  "Sorry! The computer guessed #{num}"
          break
